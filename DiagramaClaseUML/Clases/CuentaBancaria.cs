6a0db612c5659f4df3bc82bf35196fea500ae4c9
César Acosta <c3sar.adan@gmail.com>
2022-01-29 13:12:58 -0700

    Agregar archivos de proyecto.

---
+++ DiagramaClaseUML/Clases/CuentaBancaria.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagramaClaseUML.Clases
+{
+    /*
+     * La clase CuentaBancaria respresenta cuentas
+     * bancarias en las que se mantiene un saldo
+     * @autor César Acosta
+     */
+    internal class CuentaBancaria
+    {
+        //--------------------------------PROPIEDDADES------------------------------------
+        //Numero de cuenta
+        private string numero;
+
+        //Saldo en pesos de la cuenta
+        private float saldo;
+
+        //private bool pico;
+
+        //------------------------------------METODOS O FUNCIONES--------------------------------
+
+        //Constructor de la clase CuentaBancaria
+        public CuentaBancaria(string numero, float saldo)
+        {
+            this.numero = numero;
+            this.saldo = saldo;
+        }
+
+        public float ObtenerSaldo()
+        {
+            return saldo;
+        }
+
+        public void Ingresar(float cantidad)
+        {
+            saldo = saldo + cantidad;
+            //saldo += cantidad;
+        }
+
+        public string Retirar(float cantidad)
+        {
+            if(cantidad > saldo)
+            {
+                return "Saldo insuficiente";
+            }
+            //saldo = saldo - cantidad;
+            saldo -= cantidad;
+            return "Su saldo es de " + saldo;
+        }
+
+        //--------------------------------------------------------------------
+
+    }
+}
---
+++ DiagramaClaseUML/Program.cs
@@ -0,0 +1,30 @@
+﻿using DiagramaClaseUML.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagramaClaseUML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Alumno Erick
+            CuentaBancaria cuentaBancaria = new CuentaBancaria(numero : "21540026", saldo: 500000);
+            float saldoActual = cuentaBancaria.ObtenerSaldo();
+            Console.WriteLine(saldoActual);
+            //Console.WriteLine(cuentaBancaria.ObtenerSaldo());
+            Console.WriteLine("Retira 150 000");
+            Console.WriteLine(cuentaBancaria.Retirar(150000));
+            Console.WriteLine("Depositar 250 000");
+            cuentaBancaria.Ingresar(250000);
+            Console.WriteLine(cuentaBancaria.ObtenerSaldo());
+
+
+
+            Console.ReadKey(); 
+        }
+    }
+}
